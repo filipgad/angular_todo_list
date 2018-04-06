43e4bbd5197d1264e5dee52d9eaeda0113ce5a4a
Filip Godziński <godzinski.filip@gmail.com>
2018-04-06 11:13:28 +0200

    add, delete, done - task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    newTask: string;
+    tasksList: Array<string> = [];
+    tasksDone: Array<string> = [];
+
+    onAdd() {
+        this.tasksList.push(this.newTask);
+        this.newTask = '';
+    }
+
+    onDelete(task: string) {
+        this.tasksList = this.tasksList.filter( e => e !== task );
+    }
+
+    onDone(task: string) {
+        this.tasksDone.push(task);
+        this.onDelete(task);
+    }
 }
